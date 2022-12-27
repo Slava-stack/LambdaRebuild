edd357cf4eeea2a9a5385d3350efba2010797fac
Slava <minielephant16@gmail.com>
2022-12-27 17:47:59 +0200

    alteration

--- Auth_Backend/config.js
+++ Auth_Backend/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    secret_jwt_access: "SECRET_KEY_RANDOM",
-    secret_jwt_refresh: "SECRET_KEY_RANDOM_REFRESH"
-}
+  secret_jwt_access: "SECRET_KEY_RANDOM",
+  secret_jwt_refresh: "SECRET_KEY_RANDOM_REFRESH",
+};
--- Auth_Backend/middlewares/authMiddleware.js
+++ Auth_Backend/middlewares/authMiddleware.js
@@ -1,17 +1,20 @@
-const jwt = require('jsonwebtoken');
-const {secret_jwt_access} = require('../config')
+const jwt = require("jsonwebtoken");
+const { secret_jwt_access } = require("../config");
 module.exports = (req, res, next) => {
-    try {
-        const request_num = req.params.request_number;
-        const token = req.headers.authorization.split(' ')[1]
-        if (!token)
-            return res.status(401).json({message: "User is not authorized"})
-        const decodedData = jwt.verify(token, secret_jwt_access);
-        const {email, password} = decodedData;
-        res.status(200).json({request_num, data: {username: `${email} ${password}`}});
-        next();
-    } catch (e) {
-        console.log(e);
-        return res.status(401).json({message: "User is not authorized"})
+  try {
+    const request_num = req.params.request_number;
+    const token = req.headers.authorization.split(" ")[1];
+    if (!token) {
+      return res.status(401).json({ message: "User is not authorized" });
     }
-}
+    const decodedData = jwt.verify(token, secret_jwt_access);
+    const { email, password } = decodedData;
+    res
+      .status(200)
+      .json({ request_num, data: { username: `${email} ${password}` } });
+    next();
+  } catch (e) {
+    console.log(e);
+    return res.status(401).json({ message: "User is not authorized" });
+  }
+};
