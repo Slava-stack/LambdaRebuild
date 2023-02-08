7c9c58c72d270b10332141a8eaf3c430e66f24ce
Slava <minielephant16@gmail.com>
2023-02-08 05:13:16 +0200

    refactoring

--- Crypto_Rest_API_Backend_TS/src/interfaces/types.ts
+++ Crypto_Rest_API_Backend_TS/src/interfaces/types.ts
@@ -1,56 +1,38 @@
-type filteredResult = {
+export type filteredResult = {
   coin_name: string;
   average_price: number;
   dateTimeStamp: string;
   markets?: string;
 };
-type symbolAndPrice = { symbol: string; price: number };
-type symbolPriceApi = { symbol: string; price: number; api: string };
-type getCoinInterface = {
+export type symbolAndPrice = { symbol: string; price: number };
+export type symbolPriceApi = { symbol: string; price: number; api: string };
+export type getCoinInterface = {
   coinName: string;
   market: string;
   timePeriodStart: string;
   timePeriodFinish: string;
 };
-type coinMarketCoin = {
+export type coinMarketCoin = {
   symbol: string;
   quote: { USD: { price: number } };
 };
-type coinMarketResponse = {
-  data: { symbol: string; quote: { USD: { price: number } } }[];
-};
-type coinBaseCoin = { id: string; name: string; min_size: string };
-type coinPaprkaCoin = {
+export type coinBaseCoin = { id: string; name: string; min_size: string };
+export type coinPaprkaCoin = {
   symbol: string;
   quotes: { USD: { price: number } };
 };
-type averagePriceInterface = { [symbol: string]: number[] };
-type apiInterface = { [symbol: string]: string };
-type coinStatResponse = { coins: { symbol: string; price: number }[] };
-type kuCoinResponse = { data: { [key: string]: string } };
-type coinBaseResponse = { rates: { [key: string]: string } };
-
-type coinPapricaResponse = {
+export type averagePriceInterface = { [symbol: string]: number[] };
+export type apiInterface = { [symbol: string]: string };
+export type coinMarketResponse = {
+  data: { symbol: string; quote: { USD: { price: number } } }[];
+};
+export type coinStatResponse = { coins: { symbol: string; price: number }[] };
+export type kuCoinResponse = { data: { [key: string]: string } };
+export type coinBaseResponse = { rates: { [key: string]: string } };
+export type coinPapricaResponse = {
   data: {
     symbol: string;
     quotes: { USD: { price: number } };
   }[];
 };
-type coinBaseCurrenciesResp = { data: coinBaseCoin[] };
-export {
-  filteredResult,
-  symbolAndPrice,
-  symbolPriceApi,
-  getCoinInterface,
-  coinMarketCoin,
-  coinBaseCoin,
-  coinPaprkaCoin,
-  averagePriceInterface,
-  apiInterface,
-  coinStatResponse,
-  kuCoinResponse,
-  coinPapricaResponse,
-  coinMarketResponse,
-  coinBaseCurrenciesResp,
-  coinBaseResponse,
-};
+export type coinBaseCurrenciesResp = { data: coinBaseCoin[] };
