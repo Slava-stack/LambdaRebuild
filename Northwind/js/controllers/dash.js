709253c35bc2bc96ac83a11fe6cdf1f6900b8f7d
Slava <minielephant16@gmail.com>
2023-06-08 20:42:45 +0300

    still testing dash

--- Northwind/js/controllers/dash.js
+++ Northwind/js/controllers/dash.js
@@ -1,32 +1,63 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+var __awaiter =
+  (this && this.__awaiter) ||
+  function (thisArg, _arguments, P, generator) {
+    function adopt(value) {
+      return value instanceof P
+        ? value
+        : new P(function (resolve) {
+            resolve(value);
+          });
+    }
     return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
+      function fulfilled(value) {
+        try {
+          step(generator.next(value));
+        } catch (e) {
+          reject(e);
+        }
+      }
+      function rejected(value) {
+        try {
+          step(generator["throw"](value));
+        } catch (e) {
+          reject(e);
+        }
+      }
+      function step(result) {
+        result.done
+          ? resolve(result.value)
+          : adopt(result.value).then(fulfilled, rejected);
+      }
+      step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
-};
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+  };
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Dash = void 0;
 const getCountryCode_1 = __importDefault(require("../helpers/getCountryCode"));
+console.log(getCountryCode_1);
 const getIATA_1 = __importDefault(require("../helpers/getIATA"));
-const Dash = (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+console.log(getIATA_1);
+const Dash = (req, res, next) =>
+  __awaiter(void 0, void 0, void 0, function* () {
     const ip = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
     if (ip && !Array.isArray(ip)) {
-        try {
-            const code = yield (0, getCountryCode_1.default)(ip);
-            if (code) {
-                res.send({ countryCode: code, colo: yield (0, getIATA_1.default)(code) });
-            }
+      try {
+        const code = yield (0, getCountryCode_1.default)(ip);
+        if (code) {
+          res.send({
+            countryCode: code,
+            colo: yield (0, getIATA_1.default)(code),
+          });
         }
-        catch (err) {
-            next(err);
-        }
+      } catch (err) {
+        next(err);
+      }
     }
-});
+  });
 exports.Dash = Dash;
--- Northwind/src/helpers/getCountryCode.ts
+++ Northwind/src/helpers/getCountryCode.ts
@@ -2,13 +2,11 @@ import fsp from "fs/promises";
 
 export default async function getCountryCode(ip: string) {
   const decIp = ipToDecIp(ip);
-  console.log(decIp);
   const data = await fsp.readFile("./IP2LOCATION-LITE-DB1.CSV", "utf8");
   return findIpRowAccordingDecIp(data, decIp);
 }
 
 function ipToDecIp(ip: string) {
-  console.log(ip)
   const octetArr: string[] = [];
   ip.split(".").forEach((el) => {
     octetArr.push("0".repeat(8 - (+el).toString(2).length) + (+el).toString(2));
